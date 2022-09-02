faa8802219294aa819b5b9853b32d668b809ea7e
NxxhL <89213023+chifouu65@users.noreply.github.com>
2022-09-02 22:57:13 +0200

    x

--- components/Navbar.js
+++ components/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar() {
             <nav>
                 <Image src="/rupee.png" width={50} height={48}/>
                 <h1>Gaming Vibes</h1>
-                { authReady && (
+                { !authReady && (
                     <ul>
                         <li><Link href="/"><a>Home</a></Link></li>
                         <li><Link href="/guides"><a>Guides</a></Link></li>
