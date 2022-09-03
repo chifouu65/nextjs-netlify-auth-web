ab4637c178693f32cc5237c7a85cc7e2fec3a491
NxxhL <89213023+chifouu65@users.noreply.github.com>
2022-09-03 05:07:18 +0200

    .

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,8 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.home}>
-       <h2>Welcome to Gaming Vibes</h2>
       <div>
-          <p>website made with signup netlify api
-
-              you cannot see the content of the blog page if you do not have login / signup</p>
+          <p>website made with signup netlify api you cannot see the content of the blog page if you do not have login / signup</p>
       </div>
     </div>
   )
