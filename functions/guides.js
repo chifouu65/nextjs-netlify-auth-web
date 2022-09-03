e2d68e859642319711e573ab665119198f48e2d8
NxxhL <89213023+chifouu65@users.noreply.github.com>
2022-09-03 04:43:30 +0200

    netlify patch
    add iff user is not connect is as not content
    
    else he see content

--- functions/guides.js
+++ functions/guides.js
@@ -1,13 +1,19 @@
 exports.handler = async (event, context) => {
     const guides = [
-        { title: 'Le Lorem Ipsum est simplement du faux texte employé dans la composi', author: 'mario'},
-        { title: 'ge avant impression. Le Lorem Ipsum est le faux texte standard deLe', author: 'luigi'},
-        { title: 'Le LLe Lorem Ipsum est simplement du faux texte employé dans la co', author: 'bowzer'},
+        {title: 'Le Lorem Ipsum est simplement du faux texte employé dans la composi', author: 'mario'},
+        {title: 'ge avant impression. Le Lorem Ipsum est le faux texte standard deLe', author: 'luigi'},
+        {title: 'Le LLe Lorem Ipsum est simplement du faux texte employé dans la co', author: 'bowzer'},
     ]
 
-    //return
+    if (context.clientContext.user) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify(guides)
+        }
+    }
+
     return {
-        statusCode: 200,
-        body: JSON.stringify(guides)
+        statusCode: 401,
+        body: JSON.stringify({mssg: 'You must be loggin in to see this !'})
     }
 }
--- pages/guides.js
+++ pages/guides.js
@@ -1,17 +1,26 @@
 import styles from '../styles/Guides.module.css'
-import {useEffect} from "react";
+import {useContext, useEffect} from "react";
+import AuthContext from "../stores/authContext";
 
 export default function Guides() {
 
-    useEffect(() => {
-        fetch('/.netlify/functions/guides')
-            .then(res => res.json())
-            .then(guides => console.log(guides))
-    }, [])
+    const { user, authReady } = useContext(AuthContext)
 
-  return (
-    <div className={styles.guides}>
-      <h2>All Guides</h2>
-    </div> 
-  )
+    useEffect(() => {
+        if (authReady) {
+            fetch('/.netlify/functions/guides', user && {
+                headers: {
+                    Authorization: 'Bearer ' + user.token.access_token
+                }
+            })
+                .then(res => res.json())
+                .then(guides => console.log(guides))
+        }
+    }, [user])
+
+    return (
+        <div className={styles.guides}>
+            <h2>All Guides</h2>
+        </div>
+    )
 }
